a2d51641d354e25e2a515a41c198a597db6f57af
nanitKh <nanit.khanna@artesian.io>
2025-07-03 18:24:59 +0530

    Basic String Matching

---
+++ StringMatching/Program.cs
@@ -0,0 +1,29 @@
+﻿int a = 10;
+
+if(a is > 5)
+{
+    Console.WriteLine("Greater than 5");
+}
+
+
+int? maybe = null;
+if (maybe is int number)
+{
+    Console.WriteLine(number);
+}
+
+else
+{
+    Console.WriteLine("No value");  
+}
+
+
+int? maybe2 = 10;
+if (maybe2 is int number2)
+{
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine("No value");
+}
